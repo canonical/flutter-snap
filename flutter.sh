2e1c2c07a04829ccd5b332e063e6f7ad7c2612d7
Ken VanDine <ken.vandine@canonical.com>
2020-03-21 00:38:59 -0400

    init

---
+++ flutter.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+FLUTTER=$SNAP_USER_COMMON/flutter/bin/flutter
+
+if [ ! -d "$SNAP_USER_COMMON/flutter" ]; then
+    echo "Initializing Flutter"
+    git clone https://github.com/flutter/flutter.git $SNAP_USER_COMMON/flutter
+    if [ -x $FLUTTER ]; then
+      echo "Flutter initialized"
+      $FLUTTER --version
+      if [ "$#" -eq 0 ]; then
+        exit
+      fi
+    else
+      echo "Flutter initialization failed"
+    fi
+fi
+
+if [ ! -x $FLUTTER ]; then
+    echo "Could not find working copy of Flutter"
+    exit
+fi
+
+$FLUTTER "$@"
