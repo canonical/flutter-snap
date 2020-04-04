25197b6b488a812c850e61484263b87c7f9ded23
Ken VanDine <ken@vandine.org>
2020-04-04 00:48:01 -0400

    Pull stable tarball rather than git clone. Added --reset arg to wipe out existing repo and download again

--- flutter.sh
+++ flutter.sh
@@ -2,9 +2,41 @@
 
 FLUTTER=$SNAP_USER_COMMON/flutter/bin/flutter
 
-if [ ! -d "$SNAP_USER_COMMON/flutter" ]; then
-    echo "Initializing Flutter"
+reset_install () {
+  echo "Resetting flutter repository"
+  rm -rf $SNAP_USER_COMMON/flutter
+  download_flutter
+}
+
+# Download stable via tarball
+download_flutter () {
+  # Determine URL for latest stable release
+  cd $SNAP_USER_COMMON
+  curl -s -o releases_linux.json https://storage.googleapis.com/flutter_infra/releases/releases_linux.json
+  base_url=$(cat releases_linux.json | jq -r '.base_url')
+  stable=$(cat releases_linux.json | jq -r '.current_release' | jq '.stable')
+  archive=$(cat releases_linux.json | jq -r --arg stable "$stable" '.releases[] | select(.hash=='$stable').archive')
+  url=$base_url/$archive
+  echo "Downloading $url"
+  curl -s -o latest_stable.tar.xz --user-agent 'Flutter SDK Snap' $url
+  tar xf latest_stable.tar.xz
+  [ -d "$SNAP_USER_COMMON/flutter/.git" ] && rm -f latest_stable.tar.xz releases_linux.json
+}
+
+# Download stable via git
+download_flutter_git () {
     git clone https://github.com/flutter/flutter.git -b stable $SNAP_USER_COMMON/flutter
+}
+
+if [ "$1" == "--reset" ];
+then
+  reset_install
+  exit
+fi
+
+if [ ! -d "$SNAP_USER_COMMON/flutter/.git" ]; then
+    echo "Initializing Flutter"
+    download_flutter
     if [ -x $FLUTTER ]; then
       echo "Flutter initialized"
       $FLUTTER --version
