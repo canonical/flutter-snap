e9f0fd567e57ea5335fb209edcb0f0ff7c42606c
Marcus Tomlinson <marcus.tomlinson@canonical.com>
2020-06-22 16:26:16 +0100

    Whoops, export env vars

--- env.sh
+++ env.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-SNAP=/snap/flutter/current
-SNAP_USER_COMMON=/home/marcustomlinson/snap/flutter/common
-SNAPCRAFT_ARCH_TRIPLET=x86_64-linux-gnu
-SNAPCRAFT_PROJECT_NAME=flutter
+export SNAP=/snap/flutter/current
+export SNAP_USER_COMMON=/home/marcustomlinson/snap/flutter/common
+export SNAPCRAFT_ARCH_TRIPLET=x86_64-linux-gnu
+export SNAPCRAFT_PROJECT_NAME=flutter
 
-PATH=${SNAP}/usr/bin:${SNAP}/bin:$PATH
-GIT_EXEC_PATH=${SNAP}/usr/lib/git-core
-GIT_CONFIG_NOSYSTEM=1
-CURL_CA_BUNDLE=/snap/core18/current/etc/ssl/certs/ca-certificates.crt
-GIT_SSL_CAINFO=/snap/core18/current/etc/ssl/certs/ca-certificates.crt
-CPLUS_INCLUDE_PATH=${SNAP}/usr/include/${SNAPCRAFT_ARCH_TRIPLET}/c++/8:${SNAP}/usr/include/c++/8:${SNAP}/usr/include:${SNAP}/usr/include/${SNAPCRAFT_ARCH_TRIPLET}:${SNAP}/usr/include/c++/8
-LIBRARY_PATH=${SNAP}/usr/lib/gcc/${SNAPCRAFT_ARCH_TRIPLET}/8:${SNAP}/usr/lib/${SNAPCRAFT_ARCH_TRIPLET}:${SNAP}/usr/lib
-LDFLAGS="${LDFLAGS} -L${SNAP}/usr/lib/gcc/${SNAPCRAFT_ARCH_TRIPLET}/8 -L${SNAP}/usr/lib/${SNAPCRAFT_ARCH_TRIPLET} -L${SNAP}/usr/lib/ ${LDFLAGS}"
-PKG_CONFIG_PATH=/snap/${SNAPCRAFT_PROJECT_NAME}/current/usr/lib/pkgconfig:/snap/${SNAPCRAFT_PROJECT_NAME}/current/usr/share/pkgconfig:/snap/${SNAPCRAFT_PROJECT_NAME}/current/usr/lib/${SNAPCRAFT_ARCH_TRIPLET}/pkgconfig:$PKG_CONFIG_PATH:/usr/lib/${SNAPCRAFT_ARCH_TRIPLET}/pkgconfig:/usr/share/pkgconfig:/usr/lib/pkgconfig
+export PATH=${SNAP}/usr/bin:${SNAP}/bin:$PATH
+export GIT_EXEC_PATH=${SNAP}/usr/lib/git-core
+export GIT_CONFIG_NOSYSTEM=1
+export CURL_CA_BUNDLE=/snap/core18/current/etc/ssl/certs/ca-certificates.crt
+export GIT_SSL_CAINFO=/snap/core18/current/etc/ssl/certs/ca-certificates.crt
+export CPLUS_INCLUDE_PATH=${SNAP}/usr/include/${SNAPCRAFT_ARCH_TRIPLET}/c++/8:${SNAP}/usr/include/c++/8:${SNAP}/usr/include:${SNAP}/usr/include/${SNAPCRAFT_ARCH_TRIPLET}:${SNAP}/usr/include/c++/8
+export LIBRARY_PATH=${SNAP}/usr/lib/gcc/${SNAPCRAFT_ARCH_TRIPLET}/8:${SNAP}/usr/lib/${SNAPCRAFT_ARCH_TRIPLET}:${SNAP}/usr/lib
+export LDFLAGS="${LDFLAGS} -L${SNAP}/usr/lib/gcc/${SNAPCRAFT_ARCH_TRIPLET}/8 -L${SNAP}/usr/lib/${SNAPCRAFT_ARCH_TRIPLET} -L${SNAP}/usr/lib/ ${LDFLAGS}"
+export PKG_CONFIG_PATH=/snap/${SNAPCRAFT_PROJECT_NAME}/current/usr/lib/pkgconfig:/snap/${SNAPCRAFT_PROJECT_NAME}/current/usr/share/pkgconfig:/snap/${SNAPCRAFT_PROJECT_NAME}/current/usr/lib/${SNAPCRAFT_ARCH_TRIPLET}/pkgconfig:$PKG_CONFIG_PATH:/usr/lib/${SNAPCRAFT_ARCH_TRIPLET}/pkgconfig:/usr/share/pkgconfig:/usr/lib/pkgconfig
