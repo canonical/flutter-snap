d37eef811a1ad706b5436687fcf80b99fb9cfd40
Marcus Tomlinson <marcus.tomlinson@canonical.com>
2020-06-30 08:45:21 +0100

    Don't overwrite $SNAP if already set

--- env.sh
+++ env.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 export SNAPCRAFT_ARCH_TRIPLET=<SNAPCRAFT_ARCH_TRIPLET>
-export SNAPCRAFT_PROJECT_NAME=<SNAPCRAFT_PROJECT_NAME>
 
-export SNAP=/snap/$SNAPCRAFT_PROJECT_NAME/current
-export SNAP_USER_COMMON=$HOME/snap/$SNAPCRAFT_PROJECT_NAME/common
+if [ -z "$SNAP" ]
+then
+  export SNAPCRAFT_PROJECT_NAME=<SNAPCRAFT_PROJECT_NAME>
+  export SNAP=/snap/$SNAPCRAFT_PROJECT_NAME/current
+  export SNAP_USER_COMMON=$HOME/snap/$SNAPCRAFT_PROJECT_NAME/common
+fi
 
 export PATH=$SNAP/usr/bin:$SNAP/bin:$PATH
 export GIT_EXEC_PATH=$SNAP/usr/lib/git-core
@@ -14,4 +17,4 @@ export GIT_SSL_CAINFO=/snap/core18/current/etc/ssl/certs/ca-certificates.crt
 export CPLUS_INCLUDE_PATH=$SNAP/usr/include/$SNAPCRAFT_ARCH_TRIPLET/c++/8:$SNAP/usr/include/c++/8:$SNAP/usr/include:$SNAP/usr/include/$SNAPCRAFT_ARCH_TRIPLET:$SNAP/usr/include/c++/8
 export LIBRARY_PATH=$SNAP/usr/lib/gcc/$SNAPCRAFT_ARCH_TRIPLET/8:$SNAP/usr/lib/$SNAPCRAFT_ARCH_TRIPLET:$SNAP/usr/lib
 export LDFLAGS="$LDFLAGS -L$SNAP/usr/lib/gcc/$SNAPCRAFT_ARCH_TRIPLET/8 -L$SNAP/usr/lib/$SNAPCRAFT_ARCH_TRIPLET -L$SNAP/usr/lib/ $LDFLAGS"
-export PKG_CONFIG_PATH=/snap/$SNAPCRAFT_PROJECT_NAME/current/usr/lib/pkgconfig:/snap/$SNAPCRAFT_PROJECT_NAME/current/usr/share/pkgconfig:/snap/$SNAPCRAFT_PROJECT_NAME/current/usr/lib/$SNAPCRAFT_ARCH_TRIPLET/pkgconfig:$PKG_CONFIG_PATH:/usr/lib/$SNAPCRAFT_ARCH_TRIPLET/pkgconfig:/usr/share/pkgconfig:/usr/lib/pkgconfig
+export PKG_CONFIG_PATH=$SNAP/usr/lib/pkgconfig:$SNAP/usr/share/pkgconfig:$SNAP/usr/lib/$SNAPCRAFT_ARCH_TRIPLET/pkgconfig:$PKG_CONFIG_PATH:/usr/lib/$SNAPCRAFT_ARCH_TRIPLET/pkgconfig:/usr/share/pkgconfig:/usr/lib/pkgconfig
