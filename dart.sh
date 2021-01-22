7abb1aee06c57b6fefa41efc3611dc56c7a17b0f
Ken VanDine <ken.vandine@canonical.com>
2021-01-22 08:10:15 -0500

    Added dart wrapper, fixes #25

---
+++ dart.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+SCRIPT_DIR=`dirname $0`
+. $SCRIPT_DIR/env.sh
+
+DART=$SNAP_USER_COMMON/flutter/bin/dart
+
+if [ ! -d "$SNAP_USER_COMMON/flutter/.git" ]; then
+    echo "Flutter not initialized, please run the flutter command once"
+    exit
+fi
+
+if [ ! -x $DART ]; then
+    echo "Could not find working copy of Dart"
+    exit
+fi
+
+# Always copy over the bootstrap script in case of changes
+cp $SCRIPT_DIR/env.sh $SNAP_USER_COMMON/flutter/bin/internal/bootstrap.sh
+$DART "$@"
