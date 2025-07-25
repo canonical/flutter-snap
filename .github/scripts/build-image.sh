75198ab72efc7e278ab16c979b2467d15eb41a12
Ken VanDine <ken.vandine@canonical.com>
2025-07-25 14:23:13 +0200

    Bump timeout to see if it allows enough time for snapd to start

--- .github/scripts/build-image.sh
+++ .github/scripts/build-image.sh
@@ -25,7 +25,7 @@ docker run \
     -d snapimg
 
 # wait for snapd to start
-TIMEOUT=600
+TIMEOUT=1800
 SLEEP=0.1
 echo -n "Waiting up to $(($TIMEOUT/10)) seconds for snapd startup "
 while [ "$(docker exec snapc sh -c 'systemctl status snapd.seeded >/dev/null 2>&1; echo $?')" != "0" ]; do
